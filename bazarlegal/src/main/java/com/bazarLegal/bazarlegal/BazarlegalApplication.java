d0a4800a066ac8994f78f3a5cccf789368ec9d99
Rafael Leal Guimarães <rafaellealg18@gmail.com>
2025-04-08 22:41:22 -0300

    1#: estrutura inical de diretórios inicial do projeto

---
+++ bazarlegal/src/main/java/com/bazarLegal/bazarlegal/BazarlegalApplication.java
@@ -0,0 +1,13 @@
+package com.bazarLegal.bazarlegal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazarlegalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BazarlegalApplication.class, args);
+	}
+
+}
