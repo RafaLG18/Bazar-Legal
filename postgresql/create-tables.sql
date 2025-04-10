5cd49816f8fb3ea2005770e381e5d532fa940880
Rafael Leal Guimarães <rafaellealg18@gmail.com>
2025-04-10 10:28:04 -0300

    #1: fix database

--- postgresql/create-tables.sql
+++ postgresql/create-tables.sql
@@ -1,13 +1,12 @@
-CREATE TABLE "lote"(
+CREATE TABLE "Lote"(
     "id" INTEGER NOT NULL,
     "dataEntrega" DATE NOT NULL,
     "observacao" VARCHAR(255) NOT NULL,
     "id_orgaoDonatario" INTEGER NOT NULL,
-    "id_orgaoFiscalizador" INTEGER NOT NULL,
-    "id_produto" INTEGER NOT NULL
+    "id_orgaoFiscalizador" INTEGER NOT NULL
 );
 ALTER TABLE
-    "lote" ADD PRIMARY KEY("id");
+    "Lote" ADD PRIMARY KEY("id");
 CREATE TABLE "OrgaoFiscalizador"(
     "id" INTEGER NOT NULL,
     "nome" VARCHAR(255) NOT NULL,
@@ -28,13 +27,14 @@ ALTER TABLE
 CREATE TABLE "Produto"(
     "codigo" INTEGER NOT NULL,
     "nome" VARCHAR(255) NOT NULL,
-    "descricao" TEXT NOT NULL
+    "descricao" TEXT NOT NULL,
+    "id_lote" INTEGER NOT NULL
 );
 ALTER TABLE
     "Produto" ADD PRIMARY KEY("codigo");
 ALTER TABLE
-    "lote" ADD CONSTRAINT "lote_id_orgaodonatario_foreign" FOREIGN KEY("id_orgaoDonatario") REFERENCES "OrgaoDonatario"("id");
+    "Lote" ADD CONSTRAINT "lote_id_orgaofiscalizador_foreign" FOREIGN KEY("id_orgaoFiscalizador") REFERENCES "OrgaoFiscalizador"("id");
 ALTER TABLE
-    "lote" ADD CONSTRAINT "lote_id_orgaofiscalizador_foreign" FOREIGN KEY("id_orgaoFiscalizador") REFERENCES "OrgaoFiscalizador"("id");
+    "Lote" ADD CONSTRAINT "lote_id_orgaodonatario_foreign" FOREIGN KEY("id_orgaoDonatario") REFERENCES "OrgaoDonatario"("id");
 ALTER TABLE
-    "lote" ADD CONSTRAINT "lote_id_produto_foreign" FOREIGN KEY("id_produto") REFERENCES "Produto"("codigo");
+    "Produto" ADD CONSTRAINT "produto_id_lote_foreign" FOREIGN KEY("id_lote") REFERENCES "Lote"("id");
